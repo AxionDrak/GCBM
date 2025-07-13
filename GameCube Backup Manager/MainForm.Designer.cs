45d693a386c62ce18ad3c8d5db6679988fbfe4f7
Laete Meireles <laetemn@gmail.com>
2025-07-13 13:12:38 -0300

    Update

---
+++ GameCube Backup Manager/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GameCube_Backup_Manager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            statusStrip = new StatusStrip();
+            menuStrip = new MenuStrip();
+            arquivoToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            statusStrip.Location = new Point(0, 533);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1171, 22);
+            statusStrip.TabIndex = 0;
+            statusStrip.Text = "statusStrip";
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { arquivoToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(1171, 24);
+            menuStrip.TabIndex = 1;
+            menuStrip.Text = "menuStrip";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            arquivoToolStripMenuItem.Size = new Size(61, 20);
+            arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1171, 555);
+            Controls.Add(statusStrip);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "GameCube Backup Manager";
+            WindowState = FormWindowState.Maximized;
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip statusStrip;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem arquivoToolStripMenuItem;
+    }
+}
--- GameCube Backup Manager/MainForm.cs
+++ GameCube Backup Manager/MainForm.cs
@@ -1,8 +1,8 @@
 namespace GameCube_Backup_Manager
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
